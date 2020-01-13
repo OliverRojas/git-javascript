c45593de8dfd0483134d4532aa48f8d3fa8db154
Oliver Rojas <roja0029@algonquinlive.com>
2020-01-13 15:34:18 -0500

    Add index.html and script.js with variables and console logs

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = 'Oliver'
+console.log(`Hello, my name is ${name}.`)
+
+let number = 5
+let product = number * 2
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
+
+// Addition is +
+// Substraction is -
+// multiplication is *
